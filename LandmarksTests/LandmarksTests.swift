b186048b2970e2f9b53bd57540c0bb49e9c137e4
Lalit Rao <lalitkumaryadav9090@gmail.com>
2024-10-31 14:50:39 +0530

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Lalit Kumar on 31/10/24.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
